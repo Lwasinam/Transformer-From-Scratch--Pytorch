16dd60be05b2e199624ef7a67a69d7b11f5be36d
Lwasinam <85133730+Lwasinam@users.noreply.github.com>
2023-08-05 14:25:49 +0100

    Update train.py

--- train.py
+++ train.py
@@ -141,11 +141,12 @@ def get_ds(config):
     #Build tokenizer
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
-
+    torch.manual_seed(45)
     #Keep 90% training and 10% for validation
     train_ds_size  = int(0.9 * len(ds_raw))
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+    print(train_ds_raw)
 
 
     train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
