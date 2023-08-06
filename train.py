a386bba333401561c8ffc1c6aa67bbc033c5a8a8
Lwasinam <85133730+Lwasinam@users.noreply.github.com>
2023-08-06 10:24:36 +0100

    Update train.py

--- train.py
+++ train.py
@@ -135,7 +135,7 @@ def get_or_build_tokenizer(config, ds, lang):
 
 
 def get_ds(config):
-    ds_raw = load_dataset('opus_books', f"{config['lang_src']}-{config['lang_tgt']}", split = 'train')
+    ds_raw = load_dataset('opus100', f"{config['lang_src']}-{config['lang_tgt']}", split = 'train')
 
 
     #Build tokenizer
